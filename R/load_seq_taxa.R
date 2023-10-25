c4f35885188db612d9c1528a14dc31dfc9a18ecf
hkittner <hailiekittner18@gmail.com>
2023-10-24 18:57:54 -0700

    fix load_seq_taxa function

--- R/load_seq_taxa.R
+++ R/load_seq_taxa.R
@@ -10,10 +10,10 @@
 load_seq_taxa <- function(date = "230212"){
 
   # Load sequence abundance table
-  seqtab <- data.frame(fread(paste("../../data/raw_data/microbes/seq.table_", date, ".csv"), header = TRUE, check.names = FALSE), row.names = 1)
+  seqtab <- data.frame(fread(paste("../../data/raw_data/microbes/seq.table_", date, ".csv", sep =""), header = TRUE, check.names = FALSE), row.names = 1)
 
   # Load sequence identity table
-  taxa <- read.csv(paste("../../data/raw_data/microbes/taxa_", date, ".csv"), header = TRUE, check.names = FALSE, row.names = 1)
+  taxa <- read.csv(paste("../../data/raw_data/microbes/taxa_", date, ".csv", sep = ""), header = TRUE, check.names = FALSE, row.names = 1)
 
   # Load map file
   map_SPB <- read.csv("../../data/raw_data/microbes/map_SPB.csv", row.names = 1)
